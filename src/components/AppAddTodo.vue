6229868b1036d3cbc454d5061d2a511736e0216e
aibolkairoldayev <ak96.kz@gmail.com>
2024-04-09 19:29:13 +0500

    made add and remove func

--- src/components/AppAddTodo.vue
+++ src/components/AppAddTodo.vue
@@ -1,16 +1,54 @@
 <template>
     <section class="add-todo">
-        <button class="add-todo__show-form-button">
-        <i class="bi bi-plus-lg"></i>
+        <button v-if="!isFormVisable" class="add-todo__show-form-button" @click="openForm">
+            <i class="bi bi-plus-lg"></i>
         </button>
-        <form class="add-todo__form">
-        <button class="close-button" type="button">
-            <i class="bi bi-x"></i>
-        </button>
-        <div class="text-input text-input--focus">
-            <input class="input" />
-        </div>
-        <button class="button button--filled">Add task</button>
+        <form v-else class="add-todo__form" @submit.prevent="addTodo">
+            <button class="close-button" type="button" @click="closeForm">
+                <i class="bi bi-x"></i>
+            </button>
+            <div class="text-input text-input--focus">
+                <input class="input" v-model="todoText"/>
+            </div>
+            <button class="button button--filled">Add task</button>
         </form>
     </section>
 </template>
+
+<script lang="ts">
+import { Todo } from '@/types/Todo';
+import { defineComponent } from 'vue';
+
+interface State {
+    isFormVisable: boolean,
+    todoText: string
+}
+
+export default defineComponent({
+    data(): State {
+        return {
+            isFormVisable: false,
+            todoText:''
+        }
+    },
+    methods: {
+        openForm(){
+            this.isFormVisable = true
+        },
+        closeForm() {
+            this.isFormVisable = false
+        },
+        addTodo(){
+            this.$emit('addTodo', {
+                id: Date.now(),
+                text: this.todoText,
+                completed: false
+            })
+            this.todoText = ''
+        }
+    },
+    emits: {
+        addTodo: (todo: Todo) => todo
+    }
+})
+</script>
--- src/components/AppTodoList.vue
+++ src/components/AppTodoList.vue
@@ -1,30 +1,43 @@
 <template>
     <ul class="todo-list">
-        <AppTodoItem v-for="todo in todos" :todo="todo" :key="todo.id"/>
+        <AppTodoItem 
+            v-for="todo in todos" 
+            :todo="todo" 
+            :key="todo.id"
+            @toggle-todo="toggleTodo"
+            @remove-todo="removeTodo"
+        />
     </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { PropType, defineComponent } from 'vue';
 import AppTodoItem from './AppTodoItem.vue';
 import { Todo } from '@/types/Todo';
 
-interface State {
-    todos: Todo[]
-}
+
 
 export default defineComponent({
   components: {
     AppTodoItem
   },
-  data():State {
-    return {
-        todos:[
-            {id: 0, text:"Learn the basics of Vue", completed: true},
-            {id: 1, text:"Learn the basics of Typescript", completed: false},
-            {id: 2, text:"Become the greatest Hokage", completed: false},
-        ]
+  props: {
+    todos: {
+        type: Array as PropType<Todo[]>
     }
-  } 
+  },
+  methods:{
+    toggleTodo(id: number) {
+        this.$emit('toggleTodo', id)
+    },
+    removeTodo(id: number) {
+        this.$emit('removeTodo', id)
+    }
+  },
+  emits: {
+    toggleTodo: (id: number)=> Number.isInteger(id),
+    removeTodo: (id: number)=> Number.isInteger(id)
+  }
+ 
 })
 </script>
